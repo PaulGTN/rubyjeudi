44becc5c6c14de6583dc46368e57f725c7de5cdf
Paul.Graton <paulgtn@hotmail.fr>
2019-01-10 18:52:18 +0100

    Escalier

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,46 +1,41 @@
 # lancé de dé
 
 def roll_of_dice
-a = rand(1..6)
-end
+  a = rand(1..6)
+  return a
+  end
+  
+  # créer une pyramide
+  
+  def game(starter)
+      n = 0
+    while n < 10
+      a = roll_of_dice
+      if a ==5||6
+        n = +1
+        puts "Tu montes d'un étage"
+      elsif a == 1
+        n = -1
+        puts "Tu  descends d'un étage"
+      else a == 2 ||3 ||4
+        n = n 
+        puts "Tu ne bouges pas"
+      end
+  end 
+  end
+  
+  def fin(starter, game_length)
+      if n = 10
+    puts "Bravo tu as gagné"
+      end
+  end
+  
+  def perform
+    starter = roll_of_dice
+    game_length = game
+    fin(starter, game_length)
+  end
+  
+  perform
+  
 
-roll_of_dice
-
-# créer une pyramide
-
-def pyramid
-
-n = 1
-while n <= 10
-  puts ("* " * n).rjust(23)
-  n += 1
-end
-
-end
-
-
-def game(starter)
-
-  while qqch < 10
-    a = roll_of_dice
-    if a = (5, 6)
-      # monte d'un étage
-      puts "Tu montes d'un étage"
-    elsif a = 1
-      # descend d'un étage
-      puts "Tu  descends d'un étage"
-    else a = (2, 3, 4)
-      # BOUGE pas
-      puts "Tu ne bouges pas"
-    end
-end
-
-def fin(starter, game_length)
-  m = 10
-  puts "Bravo tu as gagné"
-end
-
-def perform
-  starter = start
-  game_length = game
-  fin(starter, game_length)
