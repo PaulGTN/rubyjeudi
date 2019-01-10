fbb31cd9e73ec32a8b0a93f3d1ca211f8c377c1e
NIcoZF <nico-7@hotmail.fr>
2019-01-10 15:15:05 +0100

    wtf pyramide sans fonctions

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -3,34 +3,29 @@
 
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 print ">"
-number = gets.to_i
+
+numbers = gets.to_i
 puts  "Voici la pyramide :"
 
-def top_pyramid
+number = numbers / 2
 i = 1
 
 while i <= number
   puts ("#" * i).rjust(1*number) + ("#" * i)
   i = i + 1
 end
-end
+
 
 # pyramide descendante
 
-def bottom_pyramid
 
   i = number
   while i >= 1
     puts ("#" * i).rjust(1*number)+ ("#" * i)
     i = i - 1
   end
-end
+
+
+
 
 # afficher la pyramid
-
-def full_pyramid
-  puts top_pyramid
-  puts bottom_pyramid
-end
-
-puts  full_pyramid
