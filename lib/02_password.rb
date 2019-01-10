4024cee402d14e9c33e4d87a39091a86ef7d8fb4
Paul.Graton <paulgtn@hotmail.fr>
2019-01-10 15:50:37 +0100

    Passwrod

---
+++ lib/02_password.rb
@@ -0,0 +1,24 @@
+def sign_up
+ puts "Give me a password"
+ password = gets.chomp
+
+end
+
+puts sign_up
+
+def login
+
+    puts "Please put your password"
+    answer = gets
+    if answer = pass
+        puts "Welcome user"
+
+    end
+    if answer !=pass
+        puts "Fuck off"
+    end
+end
+
+puts login
+
+
