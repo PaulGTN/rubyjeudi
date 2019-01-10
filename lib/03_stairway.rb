bc9b6ae07a79e3bfdab5a0af80106db252f683a1
Paul.Graton <paulgtn@hotmail.fr>
2019-01-10 22:10:19 +0100

    modif de jeu

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -4,21 +4,24 @@ def roll_of_dice
   a = rand (1..6)
   return a
   end
-  
-  # créer une pyramide
+ 
+# partie
   
   def game(starter)
       n = 0
-    while n < 10
+    while n < 10 
       a = roll_of_dice
       if a == 5|| a == 6
         n += + 1
+        puts "Tu as fait #{a}, wow !"
         puts "Tu montes d'un étage ! Tu es sur la marche #{n}"
       elsif a == 1
         n += - 1
+        puts "Tu as fait #{a}, bouh !"
         puts "Tu  descends d'un étage ! Tu es sur la marche #{n} "
       else a == 2 ||3 ||4
         n = n 
+        puts "Tu as fait #{a}, meh !"
         puts "Tu ne bouges pas ! Tu es sur la marche #{n}"
       end
   end 
@@ -31,7 +34,7 @@ def roll_of_dice
     starter = roll_of_dice
     game(starter)
   end
-  
+ 
   perform
   
 
